e97ba35949913e780cc30c64b8bea898457659fe
wenjie <wenjie.sab@gmail.com>
2026-03-06 08:50:29 +0100

    Updating resolutions based on feedback.

--- src/ServiceLayer/WorkStation.ts
+++ src/ServiceLayer/WorkStation.ts
@@ -1,11 +1,12 @@
 import '../user.js';
 import '../organization.js'
 import { model, Schema, Types, Document } from 'mongoose';
+import { IUser } from '../user.js';
 
 export interface IWorkStation{
     _id?: string;
     //organizationId: { type: Schema.Types.ObjectId, ref: 'Organization', required: true }
-    userId?: Types.ObjectId;
+    userId?: Types.ObjectId | IUser; // Recomendado para que Typescript nos acepte al popularlo
     num: number;
 }
 
--- src/ServiceLayer/WorkStationManager.ts
+++ src/ServiceLayer/WorkStationManager.ts
@@ -1,48 +1,54 @@
-import mongoose, {Document, Types} from 'mongoose';
-import './WorkStation.js';
-import { IWorkStation, WorkStationModel } from './WorkStation.js';
+import mongoose, { Document, Types } from "mongoose";
+import "./WorkStation.js";
+import { IWorkStation, WorkStationModel } from "./WorkStation.js";
 
 //const mongoose = require('mongoose');
-import cnx from '../connection.js' // aunque seria mejor hacer por inyeccion de dependencias
-import { UserModel } from '../user.js';
-import { OrganizationModel } from '../organization.js';
+import cnx from "../connection.js"; // aunque seria mejor hacer por inyeccion de dependencias
+import { IUser, UserModel } from "../user.js";
+import { OrganizationModel } from "../organization.js";
 
 // Create
-export async function Create(userId: string, num: number){
-    const ws = new WorkStationModel({
-        userId: userId,
-        num: num
-    });
+export async function Create(
+  userId: string,
+  num: number,
+): Promise<IWorkStation> {
+  const ws = new WorkStationModel({
+    userId: userId,
+    num: num,
+  });
 
-    return await ws.save();
-
+  return await ws.save();
 }
 
 // Read
-export async function GetById(id: string) {
-    return await WorkStationModel.findById({_id: id}).populate('userId');
+export async function GetById(id: string): Promise<IWorkStation | null> {
+  return await WorkStationModel.findById({ _id: id }).populate("userId");
 }
 
-export async function GetByUserId(userId:string) {
-    return await WorkStationModel.find({userId: userId });
+export async function GetByUserId(
+  userId: string,
+): Promise<IWorkStation | any | null> {
+  return await WorkStationModel.find({ userId: userId });
 }
 
 // Update
-export async function Update(id: string, changedData: IWorkStation)
-{
-    const res = await WorkStationModel.findByIdAndUpdate(id, changedData);
-    return res;
+export async function Update(
+  id: string,
+  changedData: IWorkStation,
+): Promise<IWorkStation | null> {
+  const res = await WorkStationModel.findByIdAndUpdate(id, changedData);
+  return res;
 }
 
 // Delete
-export async function Delete(id: string) {
-    const res = await WorkStationModel.findByIdAndDelete(id);
-    return res;
+export async function Delete(id: string): Promise<IWorkStation | null> {
+  const res = await WorkStationModel.findByIdAndDelete(id);
+  return res;
 }
 
-export async function ListAll(){ 
-    const res = await WorkStationModel.find().populate('userId').lean();
-    return res;
+export async function ListAll(): Promise<IWorkStation[] | null> {
+  const res = await WorkStationModel.find().populate("userId").lean();
+  return res;
 }
 
 // El retorno de las funciones que no retornan void son de tipo mongoose.LeanDocument<IUser & Required<{
@@ -50,12 +56,12 @@ export async function ListAll(){
 // }>> | null
 
 export async function Close() {
-    await cnx.disconnect();
-    console.log("Disconnecting from MongoDB");
+  await cnx.disconnect();
+  console.log("Disconnecting from MongoDB");
 }
 export async function Clear() {
-    await UserModel.deleteMany({});
-    await OrganizationModel.deleteMany({});
-    await WorkStationModel.deleteMany({});
-    console.log("Cleaning everything to have a clear start...");
+  await UserModel.deleteMany({});
+  await OrganizationModel.deleteMany({});
+  await WorkStationModel.deleteMany({});
+  console.log("Cleaning everything to have a clear start...");
 }
